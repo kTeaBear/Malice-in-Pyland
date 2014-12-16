783c608618b7f0adc45cfa5ce74fc6070a2382e8
Caitlin <ckuleck@csumb.edu>
2014-12-15 18:18:13 -0800

    Updated area ideas + added original map

--- FinalProjectPlans.py
+++ FinalProjectPlans.py
@@ -6,11 +6,13 @@ def startingArea():
 
 def areaOne():
 #Hallway leads to trap door.
-#Trap door leads to either area five or area three or area eight or area one.
+#Trap door leads to either area five or area three.
+#Red pill vs. blue pill
 
 def areaTwo():
 #Hallway leads to trapdoor and tree.
-#Trap door leads to either area four or area eight or area nine.
+#Trap door leads to either area four or area eight.
+#Green pill vs. purple pill
 
 def areaThree():
 #No picture edit option for this area.
@@ -20,18 +22,26 @@ def areaThree():
 
 def areaFour():
 #Has jewels on the floor.
-#Trap door leads into area two, area six, or area nine.
+#Trap door leads into area two, area six.
+#Option to take jewels. [If you go to area nine, critters in hanging coffins kill you for them.]
+#Taking jewels turn world gold and sen to area two.
+#Otherwise area six.
 
 def areaFive():
 #Leaves cover ground and a fire can be seen to the north.
 #A coffin rests in this area.
-#Trap foor leads to area three, eight, or nine.
+#Trap door leads to area three or nine.
 #Or you can just walk to area six.
+#Green leaf = area three
+#Red leaf = area nine
 
 def areaSix():
 #There is a fire to the south and treasure to the north.
 #You can walk to area five or area seven.
-#Trap door leads to area eight, nine, and four.
+#Trap door leads to area eight and four.
+#put out fire = trap door cannot be found and must go to area seven.
+#If treasure taken, area eight through trap door.
+#Otherwise area four through trap door.
 
 def areaSeven():
 #You turn a corner and have the option to go back to area six.
@@ -39,14 +49,19 @@ def areaSeven():
 
 def areaEight():
 #You can somehow use the treasure to get into area ten.
-#Trap door leads to area nine, six, or back to two.
+#Trap door leads to area six, or back to two.
 #You can walk to area nine.
 #There is an ocean to the west.
+#Drinking ocean water turns the world upside down and the trap door leads to area six.
 
 def areaNine():
-#There is a coffin.
+#There is a wall of hanging coffins.
+#If you have treasure from area four, you are killed.
 #You can walk along the coast to area eight.
 #Trap door leads to area five or four.
+#You see two open coffins, one higher than the other.
+#The higher one leads to area five.
+#The lower on leads to area four.
 
 def areaTen():
 #Trap door lets you out of the game.
